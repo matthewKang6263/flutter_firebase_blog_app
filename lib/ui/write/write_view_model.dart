37b000f681708a6d0b14692b3148cd8a770b7fc3
matthew <minwoo6263@gmail.com>
2024-12-05 16:58:23 +0900

    8

--- lib/ui/write/write_view_model.dart
+++ lib/ui/write/write_view_model.dart
@@ -1,5 +1,4 @@
 import 'dart:io';
-import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter_firebase_blog_app/data/model/post.dart';
 import 'package:flutter_firebase_blog_app/data/repository/post_repository.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -8,64 +7,47 @@ import 'package:image_picker/image_picker.dart';
 class WriteState {
   final bool isWriting;
   final String? imageUrl;
-  final String title;
-  final String content;
 
   WriteState({
     required this.isWriting,
     this.imageUrl,
-    required this.title,
-    required this.content,
   });
 
   WriteState copyWith({
-    bool? isWriting,
+    required bool isWriting,
     String? imageUrl,
-    String? title,
-    String? content,
   }) {
     return WriteState(
-      isWriting: isWriting ?? this.isWriting,
-      imageUrl: imageUrl ?? this.imageUrl,
-      title: title ?? this.title,
-      content: content ?? this.content,
-    );
+        isWriting: isWriting ?? this.isWriting,
+        imageUrl: imageUrl ?? this.imageUrl);
   }
 }
 
 class WriteViewModel extends StateNotifier<WriteState> {
   WriteViewModel(Post? post)
-      : super(WriteState(
-            isWriting: false,
-            title: post?.title ?? '',
-            content: post?.content ?? ''));
+      : super(WriteState(isWriting: false, imageUrl: null));
 
   final PostRepository _postRepository = PostRepository();
 
-  void updateTitle(String title) {
-    state = state.copyWith(title: title);
-  }
-
-  void updateContent(String content) {
-    state = state.copyWith(content: content);
-  }
-
-  Future<bool> insert({required String writer}) async {
+  Future<bool> insert({
+    required String writer,
+    required String title,
+    required String content,
+  }) async {
     if (state.imageUrl == null) return false;
 
     state = state.copyWith(isWriting: true);
 
     try {
       final result = await _postRepository.insert(
-        title: state.title,
-        content: state.content,
-        writer: writer,
-        imageUrl: state.imageUrl!,
-      );
+          title: title,
+          content: content,
+          writer: writer,
+          imageUrl: state.imageUrl!);
       state = state.copyWith(isWriting: false);
       return result;
     } catch (e) {
-      print('Error inserting post: $e');
+      print('Error inserting post:$e');
       state = state.copyWith(isWriting: false);
       return false;
     }
@@ -77,14 +59,11 @@ class WriteViewModel extends StateNotifier<WriteState> {
       Reference ref = storage.ref();
       Reference fileRef =
           ref.child('${DateTime.now().microsecondsSinceEpoch}_${xFile.name}');
-
       await fileRef.putFile(File(xFile.path));
-
       String imageUrl = await fileRef.getDownloadURL();
-
       state = state.copyWith(imageUrl: imageUrl);
     } catch (e) {
-      print('Error uploading image: $e');
+      print('Error uploading image:$e');
     }
   }
 }
